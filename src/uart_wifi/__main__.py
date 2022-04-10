bc3ae5006b38d424998a4a0f42e8dc6cc30d09af
Adam Outler <adamoutler@gmail.com>
2022-04-10 22:08:00 +0000

    make __main__ functional as an alternative to monox.py

--- src/uart_wifi/__main__.py
+++ src/uart_wifi/__main__.py
@@ -1,1 +1,4 @@
-"""Main executable. Unused, use scripts instead."""
+"""Main executable. Imports the monox script and causes execution."""
+from uart_wifi.scripts import monox
+
+print(f'this should be executed from "{monox.__file__}".')
