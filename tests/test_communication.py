52078ad3e3492833a60cde102aa7f6a4719213d7
Adam Outler <adamoutler@gmail.com>
2022-04-10 21:53:50 +0000

    add gostop test

--- tests/test_communication.py
+++ tests/test_communication.py
@@ -23,26 +23,34 @@ class TestComms(unittest.TestCase):
         thread = threading.Thread(target=fake_printer.start_server)
         thread.daemon = False
         thread.start()
-        print("Sleeping while fake printer starts")
-        time.sleep(3)
+        while(TestComms.port == 0):
+            print("Sleeping while fake printer starts")
+            time.sleep(0.2)
+        time.sleep(1)  # import time
         TestComms.port = fake_printer.port
         print("Fake printer assumed started")
 
     def test_connection(self):
         """test basic connection"""
-        port = TestComms.port
-        print(f"connecting to 127.0.0.1:{port}")
-        response: Iterable[MonoXStatus] = UartWifi(
-            "127.0.0.1",
-            TestComms.port
-        ).send_request(
-            "getstatus,"
-        )
+        uart_wifi: UartWifi = get_api()
+        response: Iterable[MonoXStatus] = uart_wifi.send_request("getstatus,")
         assert len(response) > 0, "No response from Fake Printer"
         assert (
             response[0].status == "stop\r\n"
         ), "Invalid response from Fake Printer"
-        print(response[0])
+
+    def test_print(self):
+        """Test Print command"""
+        uart_wifi: UartWifi = get_api()
+        response: Iterable[MonoXStatus] = uart_wifi.send_request(
+            "goprint,0.pwmb,end"
+        )
+        assert len(response) > 0, "No response from Fake Printer"
+        assert response[0].status == "OK", "Invalid response from Fake Printer"
+        response_printing = uart_wifi.send_request("getstatus\n")
+        assert response_printing[0].status == "print"
+        response_stop = uart_wifi.send_request("gostop,end\n")
+        assert(response_stop[0].status == "OK")
 
     def test_init_fail(self):
         """failure of init"""
@@ -64,6 +72,14 @@ class TestComms(unittest.TestCase):
             print(response[0].print())
         except IndexError:
             pass
+
+
+def get_api() -> UartWifi:
+    """ "Get the UartWifi device to use for testing"""
+    port = TestComms.port
+    print(f"connecting to 127.0.0.1:{port}")
+    uart_wifi: UartWifi = UartWifi("127.0.0.1", TestComms.port)
+    return uart_wifi
 
 
 TestComms.port = 62134
